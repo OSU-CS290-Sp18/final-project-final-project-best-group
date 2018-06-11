73cccd1cf2be5c909b975e62fb31d0cc2bba309d
crollt <crollt@oregonstate.edu>
2018-06-10 20:22:16 -0700

    Lots of bugs

--- public/js/AddCharacter.js
+++ public/js/AddCharacter.js
@@ -1,12 +1,7 @@
 
-var request = new XMLHttpsRequest();
-request.setRequestHeader('Content-Type', 'application/json');
+var request = new XMLHttpRequest();
 
-var requestURL = '/name/' + document.getElementById('name-text').innerText
-                    + '/' + document.getElementById('race-text').innerText
-                    + '/submit'
 
-request.open('POST', requestURL);
 
 request.addEventListener('load', function(event) {
     if (event.target.status == 200){
@@ -14,76 +9,6 @@ request.addEventListener('load', function(event) {
     }
 });
 
-var stats = {
-    name: document.getElementById('name-text').innerText;
-    class: document.getElementById('class-text').innerText;
-    level: document.getElementById('level-text').innerText;
-    background: document.getElementById('background-text').innerText;
-    race: document.getElementById('race-text').innerText;
-    alignment: document.getElementById('alignment-text').innerText;
-    exp: document.getElementById('exp-text').innerText;
-
-    ac: document.getElementById('ac-text').innerText;
-    initiative: document.getElementById('initiative-text').innerText;
-    hp: document.getElementById('hp-text').innerText;
-    speed: document.getElementById('speed-text').innerText;
-
-    pbonus: document.getElementById('pbonus-text').innerText;
-    pp: document.getElementById('pp-text').innerText;
-
-    equipment: document.getElementById('equipment-text').innerText;
-    proficiencies: document.getElementById('proficiencies-text').innerText;
-    feats: document.getElementById('feats-text').innerText;
-
-    strength: document.getElementById('strength-text').innerText;
-    strengthmod: (parseInt(document.getElementById('strength-text').innerText)-10)/2;
-
-    dexterity: document.getElementById('dexterity-text').innerText;
-    dexmod: (parseInt(document.getElementById('dexterity-text').innerText)-10)/2;
-
-    constitution: document.getElementById('constitution-text').innerText;
-    conmod: (parseInt(document.getElementById('strength-text').innerText)-10)/2;
-
-    intelligence: document.getElementById('intelligence-text').innerText;
-    intmod: (parseInt(document.getElementById('strength-text').innerText)-10)/2;
-
-    wisdom: document.getElementById('wisdom-text').innerText;
-    wisdommod: (parseInt(document.getElementById('strength-text').innerText)-10)/2;
-
-    charisma: document.getElementById('charisma-text').innerText;
-    charismamod: (parseInt(document.getElementById('strength-text').innerText)-10)/2;
-
-    strengthsave: document.getElementById('strength-save').checked;
-    athletics: document.getElementById('athletics').checked;
-
-    dexsave: document.getElementById('dexterity-save').checked;
-    acrobatics: document.getElementById('acrobatics').checked;
-    sleight: document.getElementById('sleight-of-hand').checked;
-    stealth: document.getElementById('stealth').checked;
-
-    consave: document.getElementById('constitution-save').checked;
-
-    intsave: document.getElementById('intelligence-save').checked;
-    arcana: document.getElementById('arcana').checked;
-    history: document.getElementById('history').checked;
-    investigation: document.getElementById('investigation').checked;
-    nature: document.getElementById('nature').checked;
-    religion: document.getElementById('religion').checked;
-
-    wisdomsave: document.getElementById('wisdom-save').checked;
-    animal: document.getElementById('animal-handling').checked;
-    insight: document.getElementById('insight').checked;
-    medicine: document.getElementById('medicine').checked;
-    perception: document.getElementById('perception').checked;
-    survival: document.getElementById('survival').checked;
-
-    charismasave: document.getElementById('charisma-save').checked;
-    deception: document.getElementById('deception').checked;
-    intimidation: document.getElementById('intimidation').checked;
-    performance: document.getElementById('performance').checked;
-    pursuasion: document.getElementById('pursuasion').checked;
-
-}
 
 function fillData(){
     document.getElementById('name-text').innerText = oldstats.name;
@@ -150,9 +75,8 @@ function fillData(){
 
 }
 
-var requestBody = JSON.strigify(stats);
 
-function checkData(){
+function checkData() {
     var n = true;
 
     if( parseInt(document.getElementById('strength-text').innerText) < 2 ||
@@ -204,11 +128,89 @@ function checkData(){
     }
 
     if(n)
-        sendData();
+        var stats = {
+            name: document.getElementById('name-text').innerText,
+            class: document.getElementById('class-text').innerText,
+            level: document.getElementById('level-text').innerText,
+            background: document.getElementById('background-text').innerText,
+            race: document.getElementById('race-text').innerText,
+            alignment: document.getElementById('alignment-text').innerText,
+            exp: document.getElementById('exp-text').innerText,
+
+            ac: document.getElementById('ac-text').innerText,
+            initiative: document.getElementById('initiative-text').innerText,
+            hp: document.getElementById('hp-text').innerText,
+            speed: document.getElementById('speed-text').innerText,
+
+            pbonus: document.getElementById('pbonus-text').innerText,
+            pp: document.getElementById('pp-text').innerText,
+
+            equipment: document.getElementById('equipment-text').innerText,
+            proficiencies: document.getElementById('proficiencies-text').innerText,
+            feats: document.getElementById('feats-text').innerText,
+
+            strength: document.getElementById('strength-text').innerText,
+            strengthmod: (parseInt(document.getElementById('strength-text').innerText)-10)/2,
+
+            dexterity: document.getElementById('dexterity-text').innerText,
+            dexmod: (parseInt(document.getElementById('dexterity-text').innerText)-10)/2,
+
+            constitution: document.getElementById('constitution-text').innerText,
+            conmod: (parseInt(document.getElementById('strength-text').innerText)-10)/2,
+
+            intelligence: document.getElementById('intelligence-text').innerText,
+            intmod: (parseInt(document.getElementById('strength-text').innerText)-10)/2,
+
+            wisdom: document.getElementById('wisdom-text').innerText,
+            wisdommod: (parseInt(document.getElementById('strength-text').innerText)-10)/2,
+
+            charisma: document.getElementById('charisma-text').innerText,
+            charismamod: (parseInt(document.getElementById('strength-text').innerText)-10)/2,
+
+            strengthsave: document.getElementById('strength-save').checked,
+            athletics: document.getElementById('athletics').checked,
+
+            dexsave: document.getElementById('dexterity-save').checked,
+            acrobatics: document.getElementById('acrobatics').checked,
+            sleight: document.getElementById('sleight-of-hand').checked,
+            stealth: document.getElementById('stealth').checked,
+
+            consave: document.getElementById('constitution-save').checked,
+
+            intsave: document.getElementById('intelligence-save').checked,
+            arcana: document.getElementById('arcana').checked,
+            history: document.getElementById('history').checked,
+            investigation: document.getElementById('investigation').checked,
+            nature: document.getElementById('nature').checked,
+            religion: document.getElementById('religion').checked,
+
+            wisdomsave: document.getElementById('wisdom-save').checked,
+            animal: document.getElementById('animal-handling').checked,
+            insight: document.getElementById('insight').checked,
+            medicine: document.getElementById('medicine').checked,
+            perception: document.getElementById('perception').checked,
+            survival: document.getElementById('survival').checked,
+
+            charismasave: document.getElementById('charisma-save').checked,
+            deception: document.getElementById('deception').checked,
+            intimidation: document.getElementById('intimidation').checked,
+            performance: document.getElementById('performance').checked,
+            pursuasion: document.getElementById('pursuasion').checked
+
+        }
+        var requestURL = '/name/' + document.getElementById('name-text').innerText
+                    + '/' + document.getElementById('race-text').innerText
+                    + '/submit'
+        request.open('POST', requestURL);
+        request.setRequestHeader('Content-Type', 'application/json');
+        sendData(JSON.stringify(stats));
 }
 
-function sendData(){
-    request.send(requestBody);
+function sendData(data){
+    request.send(data);
 }
 
-document.getElementById('submit-input').onclick = checkData;
+
+window.onload = function() {
+    document.getElementById('save-button').onclick = checkData;
+}
